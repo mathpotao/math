a4810eda84b73be7e292e26db5eca7725130a0ea
attar <math.courtieu@gmail.com>
2024-02-17 19:54:20 +0100

    17/02/2024 19h54

--- Fonctions/script/script_affines.js
+++ Fonctions/script/script_affines.js
@@ -1,10 +1,11 @@
-var tabnf = ['affines_q1', 'affines_e1', 'affines_e2'];
+var tabnf = ['affines_e1', 'affines_e2', 'affines_e3', 'affines_q1'];
 
-var tnf = ['href="affines_quiz1.html">Quiz 1</a>',
-           'href="affines_exercice1.html">Exercice 1</a>',
-           'href="affines_exercice2.html">Exercice 2</a>'];
+var tnf = ['href="affines_exercice1.html">Exercice 1</a>',
+           'href="affines_exercice2.html">Exercice 2</a>',
+           'href="affines_exercice3.html">Exercice 3</a>',
+           'href="affines_quiz1.html">Quiz 1</a>'];
 
-var nexnf = 3;
+var nexnf = 4;
 
 function fait()
 {
@@ -36,7 +37,7 @@ function fait()
         }
         if (fnf == nexnf)
         {
-            localStorage.setItem('webp1',1);
+            localStorage.setItem('affines',1);
         }
     }
     else
