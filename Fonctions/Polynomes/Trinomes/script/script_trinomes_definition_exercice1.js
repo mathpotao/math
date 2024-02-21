6da8acb94c14223c3c8ad60895ae5c097585a22d
attar <math.courtieu@gmail.com>
2024-02-21 15:59:31 +0100

    21/02/2024 15h59

---
+++ Fonctions/Polynomes/Trinomes/script/script_trinomes_definition_exercice1.js
@@ -0,0 +1,19 @@
+var prop = [
+                ["développée", "canonique", "factorisée"]
+
+            ];
+
+var rep = [
+                ["canonique"]      , 
+                ["développée"]    , 
+                ["factorisée"]    , 
+                ["canonique"]    , 
+                ["factorisée"]
+            ];
+
+var tab = [  [["se1l", "sp1"]],
+             [["se2l", "sp2"]],
+             [["se3l", "sp3"]],
+             [["se4l", "sp4"]],
+             [["se5l", "sp5"]]
+           ];
