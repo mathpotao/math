7b1c91a30b5e8e7ef3a24f3187daccc4053c59f1
attar <math.courtieu@gmail.com>
2025-02-05 21:16:54 +0100

    05/02/2025 21h16

---
+++ Seconde/Fonctions/Affines/script/script_aff_var_exo1.js
@@ -0,0 +1,36 @@
+var ta = [0, 1, 2, 3, 4, 5, 6, 7];
+
+q = "La fonction $f$ est : ";
+m1 = "Il faut repérer le signe du coefficient directeur."
+m2 = "Le coefficient directeur est nul."
+
+var tab_enonce = [
+        ["Soit $f(x)=-4x+3$", q, m1],
+        ["Soit $f(x)=2x-5$", q, m1],
+        ["Soit $f(x)=7-x$", q, m1],
+        ["Soit $f(x)=x-9$", q, m1],
+        ["Soit $f(x)=3x-4$", q, m1],
+        ["Soit $f(x)=8-3x$", q, m1],
+        ["Soit $f(x)=-4$", q, m2],
+        ["Soit $f(x)=7$", q, m2]
+    ];
+
+p1 = "strictement croissante";
+p2 = "strictement décroisante";
+p3 = "constante";
+p4 = "positive";
+p5 = "négative";
+
+var tab_prop = [
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p4],
+                [p1, p2, p3, p5]
+            ];
+
+var tab_rep = [1, 0, 1, 0, 0, 1, 2, 2];
+
